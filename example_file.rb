60c68205ed1279c6d93584cda87874e0b34ec024
Alexandra Ricardel <alexandra.ricardel@gmail.com>
2021-03-14 22:54:30 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,5 @@
+puts "Hello World"
+
+puts "hi"
+
+print "hi"
---
+++ greeting.rb
@@ -0,0 +1,43 @@
+def greeting
+  puts "Hello World"
+end
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+
+say_greeting_five_times
+
+def greeting_a_person(name)
+  puts "Hello #{name}"
+end
+
+name = "Maria"
+greeting_a_person(name)
+
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+
+greeting_programmer("Maria", "Ruby")
+
+greeting_programmer("Steven", "Elixir")
+
+def greeting(name = 'neighbor')
+  puts "Hello, #{name}!"
+end
+
+greeting ("Steven")
+
+def stylish_chef
+  best_hairstyle = "Guy Fieri"
+  return "Martha Stewart"
+  "Guy Fieri"
+end
+
+stylish_chef
+
